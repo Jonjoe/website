8984155f0662fd795de7cca366cea08dc2a1ce3b
Jonjoe Whitfield <jonjoe@Jonjoes-MacBook-Pro.local>
2020-02-10 21:17:25 +0000

    build process stage 1

---
+++ dist/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+var App = function () {
+    return (React.createElement("div", null,
+        React.createElement("p", null, "Hi From React")));
+};
+export default App;
+//# sourceMappingURL=App.js.map
