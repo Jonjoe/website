87a20cf7aef93973b918bbaeb1a117231435117a
Jonjoe Whitfield <jonjoe@Jonjoes-MacBook-Pro.local>
2020-02-10 21:51:07 +0000

    live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
   entry: './src/index.tsx',
@@ -16,5 +18,9 @@ module.exports = {
       }
     ]
   },
-  plugins: [] 
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
 }
