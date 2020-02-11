d3c8a43786e2208d36408ab758e42b87e4457241
Jonjoe Whitfield <me@jonjoe.io>
2020-02-11 16:25:35 +0000

    transitions done

--- src/components/Hatch/Hatch.compoennt.tsx
+++ src/components/Hatch/Hatch.compoennt.tsx
@@ -1,51 +1,21 @@
 import * as React from "react"
-import styled from 'styled-components'
+import * as Styled from './Hatch.styles'
 
 import { HeroButton } from "../"
 
-const Panel = styled.section`
-  background: green;
-  width: 100%;
-  flex: 1;
-`
-
-const Container = styled.div`
-  background: black;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  
-  ${Panel}:first-child {
-    background: #4a95d4;
-    //border-bottom: 5px solid red;
-  }
-
-  ${Panel}:last-child {
-    background: #3b79aa;
-    //border-top: 5px solid red;
-  }
-`
-
-const ButtonWrapper = styled.div`
-  position: fixed;
-  top: calc(50% - 40px);
-  left: 0;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-`
-
 const Hatch: React.FC = () => {
+  const [hatchOpen, setHatchOpen] = React.useState(false)
+  
   return (
-    <Container>
-      <Panel />
+    <Styled.Container>
+      <Styled.Panel position="top" open={hatchOpen} />
 
-      <ButtonWrapper>
-        <HeroButton onClick={() => null} label="Dive In"/>
-      </ButtonWrapper>
+      <Styled.ButtonWrapper hidden={hatchOpen}>
+        <HeroButton onClick={() => setHatchOpen(true)} label="Dive In"/>
+      </Styled.ButtonWrapper>
       
-      <Panel />
-    </Container>
+      <Styled.Panel position="bottom" open={hatchOpen} />
+    </Styled.Container>
   )
 }
 
---
+++ src/components/Hatch/Hatch.styles.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components"
+
+interface PanelProps {
+  open: boolean;
+  position: 'top' | 'bottom'
+}
+
+export const Panel = styled.section<PanelProps>`
+  width: 100%;
+  position: relative;
+  flex: 1;
+  transition: all 0.318s;
+  opacity: 1;
+  ${(props: PanelProps) => `${props.position}: 0px;`}
+  
+  ${(props: PanelProps) => props.open && `
+    opacity: 0;
+    ${props.position}: -300px;
+  `}
+`
+
+export const Container = styled.div`
+  background: black;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: hidden;
+  
+  ${Panel}:first-child {
+    background: #4a95d4;
+  }
+
+  ${Panel}:last-child {
+    background: #3b79aa;
+  }
+`
+
+
+interface ButtonWrapperProps {
+  hidden: boolean
+}
+
+export const ButtonWrapper = styled.div<ButtonWrapperProps>`
+  position: fixed;
+  top: calc(50% - 40px);
+  left: 0;
+  width: 100%;
+  display: flex;
+  z-index: 999;
+  justify-content: center;
+  transition: all 0.318s;
+  opacity: 1;
+
+  ${(props: ButtonWrapperProps) => props.hidden && `opacity: 0`}
+`
