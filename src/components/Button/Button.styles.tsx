232d8c74c0292b016bed8735b646c1aee5ba9e62
Jonjoe Whitfield <me@jonjoe.io>
2020-02-16 21:45:04 +0000

    thicker borders

--- src/components/Button/Button.styles.tsx
+++ src/components/Button/Button.styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Button = styled.button`
   padding: 10px 20px;
-  border: 1px solid white;
+  border: 2px solid white;
   background: none;
   color: white;
 `
