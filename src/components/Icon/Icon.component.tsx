4fceb8347e51b663c2ec791a7185ee549867a87d
Jonjoe Whitfield <me@jonjoe.io>
2020-02-16 17:11:38 +0000

    better icon component

--- src/components/Icon/Icon.component.tsx
+++ src/components/Icon/Icon.component.tsx
@@ -1,26 +1,51 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { library, IconProp } from "@fortawesome/fontawesome-svg-core";
-import { fad } from "@fortawesome/pro-duotone-svg-icons";
-import { fal } from "@fortawesome/pro-light-svg-icons";
-import { fas } from "@fortawesome/pro-solid-svg-icons";
+import {library, IconProp} from '@fortawesome/fontawesome-svg-core';
+import {fad} from '@fortawesome/pro-duotone-svg-icons';
+import {fal} from '@fortawesome/pro-light-svg-icons';
+import {fas} from '@fortawesome/pro-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-library.add(fal, fad, fas);
+import * as Styled from './Icon.styles'
+
+library.add(fal, fab, fad, fas, far);
 
 export type IconName =
-  | "question-circle"
+  | 'question-circle'
+  | 'dribbble'
+  | 'twitter'
+  | 'github'
+  | 'linkedin'
+  | 'email';
 
 export interface IconProps {
   name: IconName;
+  size?: string;
   iconColor?: string;
 }
 
 function mapNamePropToFaNames(iconName: IconName): string | string[] {
   switch (iconName) {
-    case "question-circle":
-      return ["fal", "question-circle"];
+    case 'question-circle':
+      return ['fal', 'question-circle'];
+
+    case 'dribbble':
+      return ['fab', 'dribbble'];
+
+    case 'twitter':
+      return ['fab', 'twitter'];
+
+    case 'github':
+      return ['fab', 'github'];
+
+    case 'linkedin':
+      return ['fab', 'linkedin-in'];
+
+    case 'email':
+      return ['fal', 'envelope-open-text'];
 
     default:
       return iconName;
@@ -28,18 +53,19 @@ function mapNamePropToFaNames(iconName: IconName): string | string[] {
 }
 
 const Icon: React.FC<IconProps> = (props: IconProps): JSX.Element => {
-  const { name, iconColor } = props;
+  const {name, size, iconColor} = props;
 
   const iconName = mapNamePropToFaNames(name);
 
   return (
-    <FontAwesomeIcon
-      color={iconColor}
-      size={"lg"}
-      icon={iconName as IconProp}
-    />
+    <Styled.Icon size={size}>
+      <FontAwesomeIcon
+        color={iconColor}
+        size={'lg'}
+        icon={iconName as IconProp}
+      />
+    </Styled.Icon>
   );
 };
 
 export default Icon;
-
---
+++ src/components/Icon/Icon.styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+interface IconProps {
+  size?: string
+}
+
+export const Icon = styled.div`
+  font-size: ${(props:IconProps): string => props.size? props.size : `1rem`}
+`
