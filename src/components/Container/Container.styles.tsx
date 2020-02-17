865781efb86d351fc780b756f54837ba1d42a005
Jonjoe Whitfield <me@jonjoe.io>
2020-02-17 21:44:53 +0000

    tighten content

--- src/components/Container/Container.styles.tsx
+++ src/components/Container/Container.styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 1000px;
+  max-width: 800px;
   width: 100%;
 
   @media only screen and (max-width: 600px) {
