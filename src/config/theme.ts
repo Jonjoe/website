ea4d8898f675aea46e2a3ea35bb48913bf3aa754
Jonjoe Whitfield <me@jonjoe.io>
2020-03-03 18:15:32 +0000

    h6 + other typography styles

---
+++ src/config/theme.ts
@@ -0,0 +1,17 @@
+interface Theme {
+  pallet: {
+    [label: string]: string;
+  }
+}
+
+const theme: Theme = {
+  pallet: {
+    WHITE: 'white',
+    BLUE: '#4a95d4',
+    BLACK: '#39373a',
+    RED: '#e0795f',
+    GREY: '#f8f8f8'
+  }
+}
+
+export default theme
