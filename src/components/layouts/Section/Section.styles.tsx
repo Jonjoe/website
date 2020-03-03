a49f8d4b961cf9cac5417b497297718572164d0f
Jonjoe Whitfield <me@jonjoe.io>
2020-03-03 21:14:48 +0000

    cool borders

--- src/components/layouts/Section/Section.styles.tsx
+++ src/components/layouts/Section/Section.styles.tsx
@@ -16,6 +16,43 @@ export const Container = styled.section<ContainerProps>`
     props.background
       ? `background-color: ${props.background};`
       : `background-color: transparant;`}
+
+  &:before {
+    display: block;
+    content: "";
+    height: 50px;
+    width: 50px;
+    border-top: 5px solid black;
+    border-right: 5px solid black;
+    border-bottom: 0px solid black;
+    border-left: 0px solid black;
+    position: absolute;
+    top: -10px;
+    right: -10px;
+
+    ${(props: ContainerProps) =>
+      props.background
+        ? `border-color: ${props.background};`
+        : `border-color: transparant;`}
+  }
+  &:after {
+    display: block;
+    content: "";
+    height: 50px;
+    width: 50px;
+    border-top: 0px solid black;
+    border-right: 0px solid black;
+    border-bottom: 5px solid black;
+    border-left: 5px solid black;
+    position: absolute;
+    bottom: -10px;
+    left: -10px;
+
+    ${(props: ContainerProps) =>
+      props.background
+        ? `border-color: ${props.background};`
+        : `border-color: transparant;`}
+  }
 `;
 
 export const Header = styled.header`
