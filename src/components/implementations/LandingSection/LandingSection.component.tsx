d499343b8603a6169f6f2405e5944ed67ed86304
Jonjoe Whitfield <me@jonjoe.io>
2020-03-03 14:32:19 +0000

    rework landing

--- src/components/implementations/LandingSection/LandingSection.component.tsx
+++ src/components/implementations/LandingSection/LandingSection.component.tsx
@@ -1,27 +1,18 @@
 import * as React from "react";
 import * as Styled from "./LandingSection.styles";
 
-import { Column, Caption, Button, Text, SocialLinks } from "components";
+import { Column, Centered, Text, SocialLinks } from "components";
 
 const LandingSection: React.FC = () => {
   return (
     <Styled.Container>
-      <Styled.Content>
-        <Column>
-          <Caption
-            subtitle="Web / Mobile / Maker / Hacker"
-            title="Software Engineer"
-          />
-
-          <div style={{ marginBottom: 30 }}>
-            <Button>
-              <Text.Body>Contact Me</Text.Body>
-            </Button>
-          </div>
-
-          <SocialLinks />
-        </Column>
-      </Styled.Content>
+      <Centered>
+        <Styled.Content>
+          <Text.Heading3 centered>Web / Mobile / Maker / Hacker</Text.Heading3>
+          <Text.Heading1 centered>Jonjoe Whitfield</Text.Heading1>
+        </Styled.Content>
+        <SocialLinks />
+      </Centered>
     </Styled.Container>
   );
 };
--- src/components/implementations/LandingSection/LandingSection.styles.tsx
+++ src/components/implementations/LandingSection/LandingSection.styles.tsx
@@ -1,32 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.header`
-  position: relative;
-  height: 80vh;
-  width: 100%;
-  display: flex;
-  overflow: hidden;
-  justify-content: center;
-  align-items: center;
+  height: 75vh;
 `;
 
 export const Content = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: column;
-  position: relative;
-  z-index: 800;
-  //background-color: green;
-`;
-
-export const Background = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
+  margin-bottom: 50px;
   width: 100%;
-  z-index: 200;
-  background-image: url('/images/landing.jpg');
-  filter: blur(5px) grayscale(50%);
 `;
