4f5c8688d7be942d7cc1ff699937d517e05df008
Jonjoe Whitfield <me@jonjoe.io>
2020-03-07 13:52:46 +0000

    clean up button

--- src/components/atoms/Button/Button.styles.tsx
+++ src/components/atoms/Button/Button.styles.tsx
@@ -7,12 +7,13 @@ interface StyledButtonProps {
 export const Button = styled.button<StyledButtonProps>`
   width: ${(props: StyledButtonProps) => (props.width ? props.width : `100%`)};
   padding: 10px 20px;
-  border: 1px solid white;
+  border: 5px solid white;
   background: none;
   color: white;
   > * {
     text-transform: uppercase;
     justify-content: center;
     text-align: center;
+    font-weight: 900;
   }
 `;
