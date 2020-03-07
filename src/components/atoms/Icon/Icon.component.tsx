862962173409b1b3eed0cecb94ba87728b5baa55
Jonjoe Whitfield <me@jonjoe.io>
2020-03-07 13:16:04 +0000

    more icons!

--- src/components/atoms/Icon/Icon.component.tsx
+++ src/components/atoms/Icon/Icon.component.tsx
@@ -1,111 +1,132 @@
-import * as React from 'react';
+import * as React from "react";
 
-import {library, IconProp} from '@fortawesome/fontawesome-svg-core';
-import {fad} from '@fortawesome/pro-duotone-svg-icons';
-import {fal} from '@fortawesome/pro-light-svg-icons';
-import {fas} from '@fortawesome/pro-solid-svg-icons';
-import {far} from '@fortawesome/free-regular-svg-icons';
-import {fab} from '@fortawesome/free-brands-svg-icons';
+import { library, IconProp } from "@fortawesome/fontawesome-svg-core";
+import { fad } from "@fortawesome/pro-duotone-svg-icons";
+import { fal } from "@fortawesome/pro-light-svg-icons";
+import { fas } from "@fortawesome/pro-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import * as Styled from './Icon.styles';
+import * as Styled from "./Icon.styles";
 
 library.add(fal, fab, fad, fas, far);
 
 export type IconName =
-  | 'question-circle'
-  | 'dribbble'
-  | 'twitter'
-  | 'github'
-  | 'copyright'
-  | 'code'
-  | 'alien'
-  | 'heart'
-  | 'hexagon'
-  | 'code2'
-  | 'folder'
-  | 'rocket'
-  | 'bullseye'
-  | 'bulletpoint'
-  | 'glasses'
-  | 'linkedin'
-  | 'react'
-  | 'javascript'
-  | 'nodejs'
-  | 'aws'
-  | 'email';
+  | "question-circle"
+  | "dribbble"
+  | "twitter"
+  | "github"
+  | "copyright"
+  | "code"
+  | "alien"
+  | "d10"
+  | "heart"
+  | "hexagon"
+  | "code2"
+  | "folder"
+  | "rocket"
+  | "bullseye"
+  | "bulletpoint"
+  | "glasses"
+  | "linkedin"
+  | "react-native"
+  | "react"
+  | "html"
+  | "css"
+  | "react"
+  | "typescript"
+  | "javascript"
+  | "nodejs"
+  | "aws"
+  | "email";
 
 export interface IconProps {
   name: IconName;
   size?: string;
-  iconColor?: string;
+  color?: string;
 }
 
 function mapNamePropToFaNames(iconName: IconName): string | string[] {
   switch (iconName) {
-    case 'question-circle':
-      return ['fal', 'question-circle'];
+    case "question-circle":
+      return ["fal", "question-circle"];
 
-    case 'dribbble':
-      return ['fab', 'dribbble'];
+    case "dribbble":
+      return ["fab", "dribbble"];
 
-    case 'twitter':
-      return ['fab', 'twitter'];
+    case "twitter":
+      return ["fab", "twitter"];
+
+    case "heart":
+      return ["fad", "heart"];
+
+    case "copyright":
+      return ["fal", "copyright"];
+
+    case "glasses":
+      return ["fad", "glasses"];
+
+    case "folder":
+      return ["fad", "folder-open"];
+
+    case "hexagon":
+      return ["fal", "hexagon"];
+
+    case "alien":
+      return ["fad", "alien"];
     
-    case 'heart':
-      return ['fal', 'heart'];
-    
-    case 'copyright':
-      return ['fal', 'copyright'];
+    case "d10":
+      return ["fad", "dice-d10"];
 
-    case 'glasses':
-      return ['fad', 'glasses'];
+    case "bullseye":
+      return ["fad", "bullseye-arrow"];
 
-    case 'folder':
-      return ['fad', 'folder-open'];
-    
-    case 'hexagon':
-      return ['fal', 'hexagon'];
-    
-    case 'alien':
-      return ['fad', 'alien'];
+    case "bulletpoint":
+      return ["fad", "chevron-double-right"];
 
-    case 'bullseye':
-      return ['fad', 'bullseye-arrow'];
-    
-    case 'bulletpoint':
-      return ['fad', 'chevron-double-right'];
+    case "code":
+      return ["fad", "code"];
 
-    case 'code':
-      return ['fad', 'code'];
+    case "code2":
+      return ["fad", "brackets"];
 
-    case 'code2':
-      return ['fad', 'brackets'];
+    case "aws":
+      return ["fab", "aws"];
 
-    case 'aws':
-      return ['fab', 'aws'];
+    case "javascript":
+      return ["fab", "js"];
 
-    case 'javascript':
-      return ['fab', 'js'];
+    case "html":
+      return ["fab", "html5"];
 
-    case 'nodejs':
-      return ['fab', 'node'];
+    case "css":
+      return ["fab", "css3-alt"];
 
-    case 'react':
-      return ['fab', 'react'];
+    case "react":
+      return ["fab", "react"];
 
-    case 'github':
-      return ['fab', 'github'];
+    case "typescript":
+      return ["fad", "brackets-curly"];
 
-    case 'rocket':
-      return ['fad', 'rocket-launch'];
+    case "nodejs":
+      return ["fab", "node"];
 
-    case 'linkedin':
-      return ['fab', 'linkedin-in'];
+    case "react":
+      return ["fab", "react"];
 
-    case 'email':
-      return ['fal', 'envelope-open-text'];
+    case "github":
+      return ["fab", "github"];
+
+    case "rocket":
+      return ["fad", "rocket-launch"];
+
+    case "linkedin":
+      return ["fab", "linkedin-in"];
+
+    case "email":
+      return ["fal", "envelope-open-text"];
 
     default:
       return iconName;
@@ -113,15 +134,24 @@ function mapNamePropToFaNames(iconName: IconName): string | string[] {
 }
 
 const Icon: React.FC<IconProps> = (props: IconProps): JSX.Element => {
-  const {name, size, iconColor} = props;
+  const { name, size, color } = props;
 
   const iconName = mapNamePropToFaNames(name);
+
+  if (name === "react-native") {
+    return (
+      <Styled.Icon size={size}>
+        <FontAwesomeIcon icon={["fal", "mobile-android"]} />
+        <FontAwesomeIcon icon={["fab", "react"]} />
+      </Styled.Icon>
+    );
+  }
 
   return (
     <Styled.Icon size={size}>
       <FontAwesomeIcon
-        color={iconColor}
-        size={'lg'}
+        color={color}
+        size={"lg"}
         icon={iconName as IconProp}
       />
     </Styled.Icon>
