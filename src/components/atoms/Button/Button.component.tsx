b13691022ec7e4372d28868f407daad782dd13d7
Jonjoe Whitfield <me@jonjoe.io>
2020-03-12 21:22:14 +0000

    button supports internal routing

--- src/components/atoms/Button/Button.component.tsx
+++ src/components/atoms/Button/Button.component.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import { useHistory } from "react-router-dom";
 import * as Styled from "./Button.styles";
 
 import { Text } from "components";
@@ -8,20 +8,30 @@ interface ButtonProps {
   children: string;
   width?: string;
   href?: string;
+  to?: string;
   hero?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = props => {
-  const { children, width, hero, href } = props;
+  const { children, width, hero, href, to } = props;
+  const history = useHistory();
 
   function handleOnClick(href?: string) {
-    if(href) {
-      window.open(href, '_blank')
+    if (href) {
+      window.open(href, "_blank");
+    }
+
+    if (to) {
+      history.push(to);
     }
   }
 
   return (
-    <Styled.Button width={width} hero={hero} onClick={() => handleOnClick(href)}>
+    <Styled.Button
+      width={width}
+      hero={hero}
+      onClick={() => handleOnClick(href)}
+    >
       <Text.Inline inverted={hero}>{children}</Text.Inline>
     </Styled.Button>
   );
