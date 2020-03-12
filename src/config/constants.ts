327f5617109f1d5b61845ea5acdebbffa625fecd
Jonjoe Whitfield <me@jonjoe.io>
2020-03-12 13:11:00 +0000

    support env file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,10 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 const constants = {
+  apiKeys: {
+    GDRIVE: process.env.GDRIVE_API_KEY || '',
+  },
   techLabels: {
     JAVASCRIPT: "javascript",
     NODEJS: "nodejs",
