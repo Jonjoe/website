992c2309acd993bcbd8b3008fc5a4803f2f0689e
Jonjoe Whitfield <me@jonjoe.io>
2020-03-12 19:49:48 +0000

    api constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,9 +1,17 @@
-import dotenv from "dotenv"
-dotenv.config()
+import { resolve } from "path";
+import { config } from "dotenv";
+
+config({ path: resolve(__dirname, "../../.env") });
 
 const constants = {
   apiKeys: {
-    GDRIVE: process.env.GDRIVE_API_KEY || '',
+    GDRIVE: process.env.REACT_APP_GDRIVE_API_KEY || ""
+  },
+  apiUrls: {
+    GDRIVE: "https://www.googleapis.com/drive/v3"
+  },
+  googleFolderIds: {
+    CV_FOLDER: "1_gb8kcaINJgpNDyZlA2PsKtBBJyz5c6i"
   },
   techLabels: {
     JAVASCRIPT: "javascript",
