7b334f76b9175e4c1c4fc24233541708823016c3
Jonjoe Whitfield <me@jonjoe.io>
2020-03-12 19:52:23 +0000

    get cvs from google drive

--- src/pages/Cv/Cv.page.tsx
+++ src/pages/Cv/Cv.page.tsx
@@ -1,1 +1,62 @@
+import * as React from "react";
+import { theme, constants } from "config";
 
+import { googleDrive } from "services";
+
+import { Page, Section, Text, Button, CardGrid, Card } from "components";
+
+const CvsPage: React.FC = () => {
+  const [cvs, setCvs] = React.useState([]);
+  const [cvsLoading, setCvsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    googleDrive.asyncLsFolder(constants.googleFolderIds.CV_FOLDER).then(data => {
+      console.log(data.files);
+      setCvs(data.files);
+      setCvsLoading(false);
+    });
+  }, []);
+
+  function cleanFileName(fileName: string): string {
+    const delimiter = "_";
+    return fileName.split(delimiter).slice(-1)[0];
+  }
+
+  function renderActions(downloadPdf: string) {
+    return (
+      <React.Fragment>
+        <Button href={downloadPdf}>Download PDF</Button>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <Page accent={theme.pallet.BLACK}>
+      <Section
+        title="My CVs"
+        subtitle="My Specialised CVs"
+        accent={theme.pallet.BLACK}
+      >
+        {cvsLoading ? (
+          <Text.Body inverted>Loading ...</Text.Body>
+        ) : (
+          <CardGrid>
+            {cvs.map((cv: any) => {
+              return (
+                <Card
+                  icon="folder"
+                  key={cv.id}
+                  title={cleanFileName(cv.name)}
+                  body={cv.description}
+                  actions={renderActions(cv.webViewLink)}
+                />
+              );
+            })}
+          </CardGrid>
+        )}
+      </Section>
+    </Page>
+  );
+};
+
+export default CvsPage;
