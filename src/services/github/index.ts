28418ca663a2764f3c1947d4726e838062b95bf6
Jonjoe Whitfield <me@jonjoe.io>
2020-04-03 23:17:53 +0100

    get readme as text

--- src/services/github/index.ts
+++ src/services/github/index.ts
@@ -1,7 +1,9 @@
 import asyncGetRepos from "./get-repos";
+import asyncGetReadme from "./get-readme";
 
-export const API_URL = "https://api.github.com"
+export const API_URL = "https://api.github.com";
 
 export default {
-  asyncGetRepos
+  asyncGetRepos,
+  asyncGetReadme
 };
