9adeb8735e744e3fdd5fbab726967cc0601e0acf
Jonjoe Whitfield <me@jonjoe.io>
2020-04-03 21:26:49 +0100

    create subpages

---
+++ src/pages/ProjectsIndex/ProjectsIndex.page.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { capitalize } from "lodash";
+
+import { github } from "services";
+import { theme } from "config";
+
+import { selectTagFromRepos } from "selectors";
+
+import {
+  Page,
+  Button,
+  Text,
+  Section,
+  CardGrid,
+  Card,
+  Loading
+} from "components";
+
+function ProjectsIndexPage(): JSX.Element {
+  const [repos, setRepos] = useState<any[]>([]);
+  const [reposLoading, setReposLoading] = useState<boolean>(true);
+  const [loaderActive, setLoaderActive] = useState<boolean>(true);
+
+  React.useEffect(() => {
+    setTimeout(() => setLoaderActive(false), 750);
+
+    github.asyncGetRepos().then(data => {
+      setRepos(data);
+      setReposLoading(false);
+    });
+  }, []);
+
+  function decorateTitle(title: string): string {
+    return title
+      .split("-")
+      .map(word => capitalize(word))
+      .join(" ");
+  }
+
+  function renderActions(websiteUrl: string): JSX.Element {
+    return (
+      <React.Fragment>
+        <Button href={websiteUrl}>View Demo</Button>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <Page accent={theme.pallet.BLUE}>
+      <Section
+        title="Projects"
+        subtitle="Things that I am working on or have worked on in the past."
+        accent={theme.pallet.BLUE}
+      >
+        {reposLoading || loaderActive ? (
+          <Loading accent={theme.pallet.BLUE} />
+        ) : (
+          <CardGrid animated>
+            {selectTagFromRepos(repos, "project").map(repo => (
+              <Card
+                key={repo.id}
+                title={decorateTitle(repo.name)}
+                body={repo.description}
+                icon={"rocket"}
+                externalLink={repo.html_url}
+                actions={renderActions(repo.website)}
+              />
+            ))}
+          </CardGrid>
+        )}
+      </Section>
+    </Page>
+  );
+};
+
+export default ProjectsIndexPage as React.FC;
