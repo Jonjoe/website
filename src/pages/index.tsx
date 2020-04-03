3ea41ba2d1558b2df761c0f8e73d1bd90c332f3d
Jonjoe Whitfield <me@jonjoe.io>
2020-04-03 22:36:26 +0100

    constants

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 export { default as Home } from './Home/Home.page';
 export { default as Labs } from './Labs/Labs.page';
-export { default as Projects } from './Projects/Projects.page';
+export { default as ProjectsIndex } from './ProjectsIndex/ProjectsIndex.page';
+export { default as ProjectsSingle } from './ProjectsSingle/ProjectsSingle.page';
 export { default as WorkHistory } from './WorkHistory/WorkHistory.page';
 export { default as Cv } from './Cv/Cv.page';
