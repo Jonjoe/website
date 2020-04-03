a2918c9142507a0edbee12b41675c6d0dfa65c42
Jonjoe Whitfield <me@jonjoe.io>
2020-04-03 23:51:42 +0100

    better previewer styles

--- src/components/organisms/MarkdownPreviewer/MarkdownPreviewer.component.tsx
+++ src/components/organisms/MarkdownPreviewer/MarkdownPreviewer.component.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
+import * as Styled from "./MarkdownPreviewer.styles"
+
 interface MarkdownPreviewerProps {
   content: any;
 }
 
 function MarkdownPreviewer(props: MarkdownPreviewerProps): JSX.Element {
   const { content } = props;
-  return <ReactMarkdown source={content} />;
+
+  return (
+    <Styled.Container>
+      <ReactMarkdown source={content} />
+    </Styled.Container>
+  );
 }
 
 export default MarkdownPreviewer as React.FC<MarkdownPreviewerProps>;
--- src/components/organisms/MarkdownPreviewer/MarkdownPreviewer.styles.tsx
+++ src/components/organisms/MarkdownPreviewer/MarkdownPreviewer.styles.tsx
@@ -1,3 +1,70 @@
 import styled from "styled-components";
+import { theme, constants } from "config";
 
-export const Container = styled.div``;
+export const Container = styled.article`
+  width: 100%;
+  background: white;
+  padding: 20px;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p,
+  a,
+  table,
+  tr,
+  td,
+  th,
+  ul,
+  li,
+  ol {
+    font-family: Gilroy;
+    padding: 0;
+    margin: 0;
+    text-align: left;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-weight: 900;
+    margin-top: 20px;
+    margin-bottom: 50px;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+  }
+
+  h3 {
+    font-size: 2.6rem;
+    margin-bottom: 20px;
+  }
+
+  ul {
+    margin-bottom: 20px;
+    margin-left: 20px;
+  }
+
+  table {
+    width: 100%;
+  }
+
+  p {
+    font-size: 1.2rem;
+    font-weight: 300;
+    margin-bottom: 20px;
+    > strong {
+      font-weight: 900;
+      color: ${theme.pallet.BLUE};
+    }
+  }
+`;
