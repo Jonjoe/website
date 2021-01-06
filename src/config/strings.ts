d83c02e56593ddbf1894fd938fd595efab56719e
Jonjoe Whitfield <me@jonjoe.io>
2021-01-06 10:43:47 +0000

    Update strings.ts
    dont judge me

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -1,6 +1,6 @@
 const strings = {
   INTRO:
-    "I'm <strong>Jonjoe</strong>, a 29 year old product engineer from <strong>London</strong>.",
+    "I'm <strong>Jonjoe</strong>, a 30 year old product engineer from <strong>London</strong>.",
   BULLSHIT_DISCLAIMER:
     "All of my contract roles were performed under my limited company Indee Softworks Ltd. Contact me for further clarifiation if you need it."
 };
